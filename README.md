4997acf37ba221e5efb57011c2349c9db76434d7
soyboy <sbvegan@protonmail.com>
2022-08-15 16:22:09 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Superfluid Money Router
+
+Superfluid Money Router implemented with Foundry
+
+### Hacks
+
+- I edited the `remappings.txt` to point the superfuild contracts openzeppelin dependencies (created with Hardhat) to the foundry library.
---
+++ src/MoneyRouter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import { ISuperfluid, ISuperToken } from "superfluid/interfaces/superfluid/ISuperfluid.sol";
+import { ISuperfluidToken } from "superfluid/interfaces/superfluid/ISuperfluidToken.sol";
+import {IConstantFlowAgreementV1} from "superfluid/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {CFAv1Library} from "superfluid/apps/CFAv1Library.sol";
+
+contract MoneyRouter {}
