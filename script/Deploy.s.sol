054d82403cc6e9118aaa95ff2dd9ff3396603d0c
soyboy <sbvegan@protonmail.com>
2022-08-15 21:40:44 -0700

    adding deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,11 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-// todo
+import "../src/MoneyRouter.sol";
+import { Superfluid } from "superfluid/superfluid/Superfluid.sol";
+
+/**
+ * To run the deploy script:
+ *   1. Create a .env file
+ *   2. $ source .env
+ *   3. forge script script/Deploy.s.sol:DeployScript --rpc-url $GOERLI_URL --private-key $PRIVATE_KEY --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY -vvvv
+ */
+
 contract DeployScript is Script {
-    function setUp() public {}
+    address goerliHostAddress = 0x22ff293e14F1EC3A09B137e9e06084AFd63adDF9;
+    Superfluid host = Superfluid(goerliHostAddress);
 
     function run() public {
         vm.broadcast();
+        MoneyRouter moneyRouter = new MoneyRouter(host);
+        vm.stopBroadcast();
     }
 }
